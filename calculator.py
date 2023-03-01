83d31e8acf50a5c57a1740b5cde16e1eccc740c1
Michał Kasperek <michal.kasperek2000@wp.pl>
2023-03-01 23:22:44 +0100

    calc choice stetement

--- calculator.py
+++ calculator.py
@@ -6,3 +6,19 @@ print("3. Pomnóż")
 print("4. Podziel")
 print("5. Wyjdź")
 choice = input("Wybierz numer operacji: ")
+if choice == '1':
+    result = number_1 + number_2
+    print(number_1, "+", number_2, "=", result)
+elif choice == '2':
+    result = number_1 - number_2
+    print(number_1, "-", number_2, "=", result)
+elif choice == '3':
+    result = number_1 * num2
+    print(number_1, "*", number_2, "=", result)
+elif choice == '4':
+    result = number_1 / num2
+    print(number_1, "/", number_2, "=", result)
+elif choice == '5':
+    print("Do widzenia!")
+else:
+    print("Nieprawidłowy wybór. Spróbuj ponownie.")
